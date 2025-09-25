b2695b75e7be4843831548bc1473a500b17610ed
Jayden <jaydenk3.cheung@gmail.com>
2025-09-25 15:35:40 -0700

    Intern Nathan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Utils/PIDFController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Utils/PIDFController.java
@@ -2,6 +2,30 @@ package org.firstinspires.ftc.teamcode.Experiments.Utils;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class PIDFController {
 
+public class PIDFController {
+    private double kp;
+    private double kd;
+    private double kf;
+    private double p;
+    private double d;
+    private double e;
+    private double e_last;
+    private ElapsedTime timer;
+
+    private PIDFController(double kp, double kd) {
+        this.kp = kp;
+        this.kd = kd;
+        this.kf = kf;
+        timer = new ElapsedTime();
+    }
+
+    public double Calculate(double tar, double act) {
+        e = tar - act;
+        p = kp * (e);
+        d = kd * ((e - e_last) / timer.seconds());
+        e_last = e;
+        timer.reset();
+        return p + d + kf;
+    }
 }

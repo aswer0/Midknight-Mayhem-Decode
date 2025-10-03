00d0d3bc04f58d8f8679fd2702b6d3cfa77b2199
Jayden <jaydenk3.cheung@gmail.com>
2025-10-03 16:50:31 -0700

    Intake class and methods

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Intake.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Experiments;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Experiments.Sensors;
+
+@Config
+public class Intake {
+
+    public DcMotorEx intakeMotor;
+    public Servo intakeDoor;
+    public Sensors sensors;
+
+    public Intake(HardwareMap hardwareMap, Sensors sensors) {
+        this.sensors = sensors;
+
+        intakeMotor = hardwareMap.get(DcMotorEx.class,"intakeMotor");
+        intakeDoor = hardwareMap.get(Servo.class,"intakeDoor");
+    }
+    public void motorOn() {intakeMotor.setPower(1.0);}
+    public void runIntake() {
+        int color = sensors.getFrontColor();
+
+        if (color == 1) {
+            intakeDoor.setPosition(0.75);
+        } else if (color == 2) {
+            intakeDoor.setPosition(0.25);
+        } else {
+            intakeDoor.setPosition(0.75);
+        }
+    }
+}

6b062d2f505aafc430f819a2a1657b00da54b09e
nathanli <nathanli1818@gmail.com>
2026-01-07 16:19:12 -0800

    起初，　神創造天地。 2 地是空虛混沌；深淵上一片黑暗；　神的靈運行在水面上。 3 　神說：“要有光！”就有了光。 4 　神看光是好的，他就把光暗分開了。 5 　神稱光為晝，稱暗為夜。有晚上，有早晨；這是第一日。
    6 　神說：“眾水之間要有穹蒼，把水和水分開！”事就這樣成了。 7 　神造了穹蒼，把穹蒼以下的水和穹蒼以上的水分開了。 8 　神稱穹蒼為天。有晚上，有早晨；這是第二日。
    
    9 　神說：“天下的水要聚在一處，使旱地露出來！”事就這樣成了。 10 　神稱旱地為地，稱水的聚處為海。　神看這是好的。 11 　神說：“地上要長出青草、結種子的蔬菜和結果子的樹木，各從其類，在地上的果子都包著核！”事就這樣成了。 12 於是，地上長出了青草和結種子的蔬菜，各從其類；又長出結果子的樹木，各從其類，果子都包著核。　神看這是好的。 13 有晚上，有早晨；這是第三日。
    
    14 　神說：“在天上穹蒼中，要有光體來分晝夜；這些光體要作為記號，定節令、日子和年歲； 15 它們要在天上穹蒼中發光，照耀地上！”事就這樣成了。 16 於是，　神造了兩個大光體，大的管晝，小的管夜；又造了星星。 17 　神把這些光體安放在天上穹蒼中，照耀地上， 18 管晝夜，分光暗。　神看這是好的。 19 有晚上，有早晨；這是第四日。
    
    20 　神說：“水要滋長生物；地上和天空之中，要有雀鳥飛翔！” 21 於是，　神創造了大魚和在水中滋生各種能活動的生物，各從其類；又創造了各種有翅膀的飛鳥，各從其類。　神看這是好的。 22 　神就賜福給牠們，說：“要繁衍增多，充滿海洋；雀鳥也要在地上增多！” 23 有晚上，有早晨；這是第五日。
    
    24 　神說：“地上要生出活物來，各從其類；牲畜、爬行的動物和地上的野獸，各從其類！”事就這樣成了。 25 於是，　神造了地上的野獸，各從其類；牲畜，各從其類；地上各種爬行的動物，各從其類。　神看這是好的。
    
    創造人類
    26 　神說：“我們要照著我們的形象，按著我們的樣式造人；使他們管理海裡的魚、空中的鳥、地上的牲畜，以及全地，和地上所有爬行的生物！” 27 於是，　神照著自己的形象創造人；就是照著　神的形象創造了他；他所創造的有男有女。 28 　神就賜福給他們，對他們說：“要繁衍增多，充滿這地，征服它；也要管理海裡的魚、空中的鳥和地上所有走動的生物。” 29 　神說：“看哪！我把全地上結種子的各樣蔬菜，和一切果樹上有種子的果子，都賜給你們作食物。 30 至於地上的各種野獸，空中的各種飛鳥，和地上爬行有生命的各種活物，我把一切青草蔬菜賜給牠們作食物。”事就這樣成了。 31 　神看他所造的一切都很好。有晚上，有早晨；這是第六日。

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalCode/Subsystems/Intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalCode/Subsystems/Intake/Intake.java
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode.FinalCode.Subsystems.Intake;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Experiments.Utils.PIDFController;
 import org.firstinspires.ftc.teamcode.FinalCode.Subsystems.Sensors;
 
 @Config
@@ -18,16 +23,58 @@ public class Intake {
     public double v_last;
     public static double slowSpeed = 0.4;
 
+    public double motorInterval = 1000.0; // milliseconds
+    public double motorSpeed = 0.5;
+    public double motorRPM = 1000;
+
+    //================= compression transfer =============
+
+    public double kp = 1.0;
+    public double ki = 1.0;
+    public double kd = 1.0;
+    public double kf = 1.0;
+
+    PIDFController transfer_pid;
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    public ElapsedTime timer = new ElapsedTime();
+
     public Intake(HardwareMap hardwareMap, Sensors sensors) {
         //this.sensors = sensors;
 
         intakeMotor = hardwareMap.get(DcMotorEx.class,"intakeMotor");
+        intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        transfer_pid = new PIDFController(kp, ki, kd, kf);
 //        intakeDoor = hardwareMap.get(Servo.class,"intakeDoor");
     }
     public void motorOn() {intakeMotor.setPower(INTAKE_POWER);}
     public void motorOff() {intakeMotor.setPower(0);}
     public void motorReverse() {intakeMotor.setPower(-INTAKE_POWER);}
     public void motorSlow() {intakeMotor.setPower(slowSpeed);}
+
+    public void resetTransferTimer(){
+        timer.reset();
+    }
+    public void continuousTransfer() {
+        intakeMotor.setPower(motorSpeed);
+    }
+    public void intervalTransfer() {
+        if (timer.milliseconds() >= motorInterval) {
+            intakeMotor.setPower(motorSpeed);
+
+            timer.reset();
+        }
+    }
+    public void pidTransfer(boolean verbose) {
+        double rpm = intakeMotor.getVelocity() * 60 / 2303.5;
+        double speed = transfer_pid.calculate(motorRPM, rpm);
+
+        if (verbose){
+            TelemetryPacket packet = new TelemetryPacket();
+            packet.put("Intake RPM", rpm);
+        }
+
+        intakeMotor.setPower(speed);
+    }
 
     public void runIntake() {
         //int color = sensors.getFrontColor();
@@ -40,6 +87,8 @@ public class Intake {
 //            intakeDoor.setPosition(0.75);
 //        }
     }
+
+
 
     public boolean jamDetect() {
         v = intakeMotor.getVelocity();
